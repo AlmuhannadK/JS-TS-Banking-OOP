5b0aec76d773be44a85632acfa122a43b61a411f
Almuhannad <almuhannad.khalid@gmail.com>
2024-02-29 01:12:48 +0300

    added all requirements for Branch class

--- src/Branch.js
+++ src/Branch.js
@@ -1,85 +1,30 @@
 class Branch {
   constructor(name) {
     this.name = name;
-    this.customers = [
-      {
-        name: "Ahmad",
-        id: 111,
-        transactions: [
-          {
-            amount: 600,
-            date: "1.1.2000",
-          },
-        ],
-      },
-    ];
+    this.customers = [];
   }
 
   getName() {
     return this.name;
   }
-
-  getCustomer() {
+  getCustomers() {
     return this.customers;
   }
+
   addCustomer(customer) {
-    this.customers.forEach((element) => {
-      if (customer.id === element.id) {
-        console.error("Customer already exists");
-      } else {
-        this.customers.push(customer);
-      }
-    });
+    if (this.customers.includes(customer)) {
+      console.error("Customer already exists");
+    } else {
+      this.customers.push(customer);
+    }
   }
 
   addCustomerTransaction(customerId, amount) {
-    this.customers.find((customer) => {
-      if (customer.id === customerId) {
-        let result = customer;
-        console.log(customer);
-        customer.transactions.push(amount);
-      } else {
-        console.error("transaction failed");
-      }
-    });
+    const customer = this.customers.find(
+      (customerElement) => customerElement.getId() === customerId
+    );
+    if (customer) {
+      return customer.addTransaction(amount);
+    }
   }
 }
-// class Transaction {
-//   constructor(amount, date) {
-//     this.amount = amount;
-//     this.date = date;
-//   }
-// }
-
-const customer1 = {
-  name: "Bader",
-  id: 222,
-  transactions: [
-    {
-      amount: 300,
-      date: "1.1.2000",
-    },
-  ],
-};
-const customer2 = {
-  name: "Salem",
-  id: 333,
-  transactions: [
-    {
-      amount: 800,
-      date: "1.1.2000",
-    },
-  ],
-};
-
-const transactionTest = {
-  amount: 200,
-  date: "2.2.2001",
-};
-
-const branch = new Branch("Riyadh Branch");
-branch.addCustomer(customer1);
-branch.addCustomer(customer2);
-branch.addCustomerTransaction(333, transactionTest);
-
-console.log(branch.getCustomer());
---
+++ src/index.js
@@ -0,0 +1,114 @@
+class Bank {
+  constructor(name) {
+    this.name = name;
+    this.branches = [];
+  }
+  addBranch(branch) {
+    this.branches.push(branch);
+  }
+
+  //test method, not required
+  getBranch() {
+    return this.branches;
+  }
+
+  //addCustomer(branch, customer)
+
+  //addCustomerTransaction(branch, customerId, amount)
+
+  //findBranchByName(branchName)
+
+  //checkBranch(branch)
+
+  //listCustomers(brnach)
+}
+
+class Branch {
+  constructor(name) {
+    this.name = name;
+    this.customers = [];
+  }
+
+  getName() {
+    return this.name;
+  }
+  getCustomers() {
+    return this.customers;
+  }
+
+  addCustomer(customer) {
+    if (this.customers.includes(customer)) {
+      console.error("Customer already exists");
+    } else {
+      this.customers.push(customer);
+    }
+  }
+
+  addCustomerTransaction(customerId, amount) {
+    const customer = this.customers.find(
+      (customerElement) => customerElement.getId() === customerId
+    );
+    if (customer) {
+      return customer.addTransaction(amount);
+    }
+  }
+}
+
+class Customer {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+    this.transactions = [];
+  }
+
+  getName() {
+    return this.name;
+  }
+  getId() {
+    return this.id;
+  }
+  getTransaction() {
+    return this.transactions;
+  }
+
+  getBalance() {
+    let balance = 0;
+    this.transactions.forEach((transaction) => {
+      balance += transaction.amount;
+    });
+    return balance;
+  }
+
+  addTransactions(amount) {
+    if (amount > 0) {
+      const transaction = new Transaction(amount, new Date());
+      this.transactions.push(transaction);
+    }
+  }
+}
+
+class Transaction {
+  constructor(amount, date) {
+    this.amount = amount;
+    this.date = date;
+  }
+}
+
+// const bank1 = new Bank("SAB");
+// const bank2 = new Bank("BSF");
+// const bank3 = new Bank("SNB");
+
+// const branch1 = new Branch("Riyadh");
+// const branch2 = new Branch("Jeddah");
+// const branch3 = new Branch("Khubar");
+
+// const customer1 = new Customer("khalid", 12345);
+// const customer2 = new Customer("ahmad", 6789);
+// const customer3 = new Customer("bader", 43322);
+
+// const transaction1 = new Transaction(500, "14.01.2024");
+// const transaction2 = new Transaction(250, "28.02.2024");
+// const transaction3 = new Transaction(-1200, "13.01.2024");
+// const transaction4 = new Transaction(350, "28.02.2024");
+
+//console.log(JSON.stringify(bank.getBranches(), null, 2))
